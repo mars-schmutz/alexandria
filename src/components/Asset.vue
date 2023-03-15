1f222a2b9cd5fbe5c2644c30a33b3acfb63c23b6
Marshall Schmutz <darthmars97@gmail.com>
2023-03-15 13:12:09 -0600

    get basic asset rendering started

---
+++ src/components/Asset.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+    name: "Asset",
+    props: {
+        name: String,
+    }
+}
+</script>
--- src/views/ShelfView.vue
+++ src/views/ShelfView.vue
@@ -1,6 +1,26 @@
-<script setup>
-</script>
-
 <template>
-    <p>No items.</p>
+    <p v-if="assets.length == 0">No assets in library</p>
+    <div v-else>
+        <Asset v-for="asset in assets" :key="asset.name" :name="asset.name" />
+    </div>
 </template>
+
+<script>
+import Asset from "../components/Asset.vue"
+
+export default {
+    name: "ShelfView",
+    components: {
+        Asset
+    },
+    data() {
+        return {
+            assets: window.store.get("library-shelves").then((val) => {
+                val ? this.assets = val : this.assets = []
+            })
+        }
+    },
+    mounted() {
+    }
+}
+</script>
